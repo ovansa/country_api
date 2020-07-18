ecdd679a413f833b7b187086b359aa809bf6e24e
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2020-07-18 15:53:45 +0100

    Updated code formatting based on flake8 recommendation

--- app/country/filters.py
+++ app/country/filters.py
@@ -1,9 +1,11 @@
 import django_filters
 from country.models import State
 
+
 class StateListFilter(django_filters.rest_framework.FilterSet):
-    country = django_filters.CharFilter(field_name='country__name', lookup_expr='icontains')
+    country = django_filters.CharFilter(field_name='country__name',
+                                        lookup_expr='icontains')
 
     class Meta:
         model = State
-        fields = ['country',]
+        fields = ['country', ]
--- app/country/serializers.py
+++ app/country/serializers.py
@@ -3,12 +3,15 @@ from country.models import Country, State, City
 
 
 class CitySerializer(serializers.ModelSerializer):
+    '''Serializer for City Model'''
 
     class Meta:
         model = City
         fields = ['id', 'name', 'state']
 
+
 class StateSerializer(serializers.ModelSerializer):
+    '''Serializer for State Model'''
     cities = CitySerializer(many=True, read_only=True)
     country = serializers.ReadOnlyField(source='country.name')
     # Check and add source
@@ -19,9 +22,11 @@ class StateSerializer(serializers.ModelSerializer):
 
 
 class CountrySerializer(serializers.ModelSerializer):
+    '''Serializer for Country Model'''
     # states = serializers.StringRelatedField(many=True)
     states = StateSerializer(many=True, read_only=True)
 
     class Meta:
         model = Country
-        fields = ['id', 'name', 'capital', 'alpha2code', 'alpha3code', 'region', 'states']
+        fields = ['id', 'name', 'capital', 'alpha2code', 'alpha3code',
+                  'region', 'states']
