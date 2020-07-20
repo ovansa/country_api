8c4bbc5df685bdd176f1dc76be37be2ca7594c87
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2020-07-20 23:37:04 +0100

    Updated setup.sh file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,13 +3,13 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/ovansa/country_api.git'
+PROJECT_GIT_URL='https://github.com/CHANGEME.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/country_api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv python-pip-whl supervisor nginx git postgresql-client gcc libc6-dev linux-headers-5.4.0-42-generic libc6-dev
+apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
@@ -29,15 +29,15 @@ $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_country_api.conf /etc/supervisor/conf.d/country_api.conf
+cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
-supervisorctl restart country_api
+supervisorctl restart profiles_api
 
 # Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_country_api.conf /etc/nginx/sites-available/country_api.conf
+cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/country_api.conf /etc/nginx/sites-enabled/country_api.conf
+ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
 
 echo "DONE! :)"
