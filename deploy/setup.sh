b9e323041951688c80920bc240da947e79b5aa29
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2020-07-20 23:30:11 +0100

    Updated setup.sh file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -9,7 +9,7 @@ PROJECT_BASE_PATH='/usr/local/apps/country_api'
 
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv python-pip-whl supervisor nginx git postgresql-client gcc libc6-dev linux-headers-5.4.0-42-generic postgresql-server-dev-10
+apt-get install -y python3-dev python3-venv python-pip-whl supervisor nginx git postgresql-client gcc libc6-dev linux-headers-5.4.0-42-generic postgresql-dev-12.3-r2
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
